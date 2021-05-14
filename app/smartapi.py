d9445970dd88e654de6606f91dc46b31f5f78984
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-05-14 15:32:23 -0400

    Unify style with other modules
    Also improve documentation and tweak error handling

--- app/smartapi.py
+++ app/smartapi.py
@@ -1,102 +1,100 @@
-#!/usr/bin/env python3
-'''
+"""SmartAPI registry access utility.
+
 Class for querying the SmartAPI registry for TRAPI 1.1 endpoints
-There is no caching. All gets trigger a fresh query
-'''
+There is no caching. All gets trigger a fresh query.
+"""
 
 import httpx
 
-class SmartAPI:
 
+class SmartAPI:
+    """SmartAPI."""
 
     def __init__(self):
-        self.base_url = 'http://smart-api.info/api'
-
+        """Initialize."""
+        self.base_url = "http://smart-api.info/api"
 
     def get_operations_endpoints(self):
-        '''
-        Returns a list of all endpoints that support at least one workflow operation
-        '''
-
+        """Find all endpoints that support at least one workflow operation."""
         endpoints = self.get_trapi_endpoints()
-        if endpoints is None:
-            return
 
         operations_endpoints = []
         for endpoint in endpoints:
-            if endpoint['operations'] is not None:
+            if endpoint["operations"] is not None:
                 operations_endpoints.append(endpoint)
-
-        if len(operations_endpoints) == 0:
-            return None
 
         return operations_endpoints
 
-
     def get_trapi_endpoints(self):
-        '''
-        Returns a list of all endpoints that match a query for TRAPI
-        '''
+        """Find all endpoints that match a query for TRAPI."""
+        response_content = httpx.get(
+            self.base_url + "/query?limit=1000&q=TRAPI",
+            headers={"accept": "application/json"},
+        )
 
-        response_content = httpx.get( self.base_url + '/query?limit=1000&q=TRAPI', headers={'accept': 'application/json'})
-
-        if response_content.status_code != 200:
-            return
-
-        #### Unpack the response content into a dict
-        try:
-            response_dict = response_content.json()
-        except:
-            return
+        response_content.raise_for_status()
+        response_dict = response_content.json()
 
         endpoints = []
-
-        if 'hits' in response_dict:
-            for hit in response_dict['hits']:
-                endpoint_info = { 'url': None, 'operations': None, 'version': None }
-                if 'servers' in hit:
-                    for server in hit['servers']:
-                        endpoint_info['url'] = server['url']
-                if 'info' in hit:
-                    if 'x-trapi' in hit['info']:
-                        if 'version' in hit['info']['x-trapi']:
-                            endpoint_info['version'] = hit['info']['x-trapi']['version']
-                        if 'operations' in hit['info']['x-trapi']:
-                            endpoint_info['operations'] = hit['info']['x-trapi']['operations']
-                    endpoints.append(endpoint_info)
-
-        if len(endpoints) == 0:
-            return None
+        for hit in response_dict["hits"]:
+            try:
+                url = hit["servers"][0]["url"]
+            except (KeyError, IndexError):
+                url = None
+            try:
+                version = hit["info"]["x-trapi"]["version"]
+            except KeyError:
+                version = None
+            try:
+                operations = hit["info"]["x-trapi"]["operations"]
+            except KeyError:
+                operations = None
+            endpoints.append({
+                "url": url,
+                "operations": operations,
+                "version": version,
+            })
 
         return endpoints
 
 
-
 def main():
-
-    import json
+    """Run CLI."""
     import argparse
+    import json
 
-    argparser = argparse.ArgumentParser(description='CLI testing of the ResponseCache class')
-    argparser.add_argument('--get_trapi_endpoints', action='count', help='Get a list of TRAPI 1.1 endpoints')
-    argparser.add_argument('--get_operations_endpoints', action='count', help='Get a list of TRAPI 1.1 endpoints that support operations')
+    argparser = argparse.ArgumentParser(
+        description="CLI testing of the ResponseCache class"
+    )
+    argparser.add_argument(
+        "--get_trapi_endpoints",
+        action="count",
+        help="Get a list of TRAPI endpoints",
+    )
+    argparser.add_argument(
+        "--get_operations_endpoints",
+        action="count",
+        help="Get a list of TRAPI endpoints that support operations",
+    )
     args = argparser.parse_args()
 
-    if args.get_trapi_endpoints is None and args.get_operations_endpoints is None:
+    if (
+        args.get_trapi_endpoints is None
+        and args.get_operations_endpoints is None
+    ):
         argparser.print_help()
-        exit()
+        return
 
     smartapi = SmartAPI()
 
     if args.get_trapi_endpoints:
         endpoints = smartapi.get_trapi_endpoints()
-        print(json.dumps(endpoints,sort_keys=True,indent=2))
+        print(json.dumps(endpoints, sort_keys=True, indent=2))
 
     if args.get_operations_endpoints:
         endpoints = smartapi.get_operations_endpoints()
-        print(json.dumps(endpoints,sort_keys=True,indent=2))
+        print(json.dumps(endpoints, sort_keys=True, indent=2))
 
 
 if __name__ == "__main__":
     main()
-
