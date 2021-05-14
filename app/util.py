4f8eed8efec22ee155194de7c914351c6d021f1f
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-05-14 15:38:51 -0400

    Expand drop_nulls to traverse lists

--- app/util.py
+++ app/util.py
@@ -9,3 +9,18 @@ def load_example(name):
     """Load example from JSON file."""
     with open(EXAMPLES_DIR / (name + ".json"), "r") as stream:
         return json.load(stream)
+
+
+def drop_nulls(obj):
+    """Recursively remove null-valued properties."""
+    if isinstance(obj, list):
+        return [
+            drop_nulls(el) for el in obj
+        ]
+    if isinstance(obj, dict):
+        return {
+            key: drop_nulls(value)
+            for key, value in obj.items()
+            if value is not None
+        }
+    return obj
