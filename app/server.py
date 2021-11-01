b6e3b7982c2898f78fef6451075500cbf541333f
David Folarin <dof5@cornell.edu>
2021-11-01 16:17:09 -0400

    add x-location

--- app/server.py
+++ app/server.py
@@ -33,11 +33,13 @@ openapi_args = dict(
 )
 OPENAPI_SERVER_URL = os.getenv("OPENAPI_SERVER_URL")
 OPENAPI_SERVER_MATURITY = os.getenv("OPENAPI_SERVER_MATURITY", "development")
+OPENAPI_SERVER_LOCATION = os.getenv("OPENAPI_SERVER_LOCATION", "RENCI")
 if OPENAPI_SERVER_URL:
     openapi_args["servers"] = [
         {
             "url": OPENAPI_SERVER_URL,
             "x-maturity": OPENAPI_SERVER_MATURITY,
+            "x-location": OPENAPI_SERVER_LOCATION,
         },
     ]
 
