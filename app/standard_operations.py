872d34c840d5f3124beb29e48a747c1917974453
Kenneth Morton <kenny@covar.com>
2022-01-21 10:38:31 -0500

    Operations now accurately reflect id and unique

--- app/standard_operations.py
+++ app/standard_operations.py
@@ -24,12 +24,21 @@ class StandardOperations:
         operations = {}
         for op, props in response_dict["$defs"].items():
             
-            operations[op] = {
-                "id": op,
+            properties = props.get('properties',{})
+            opid_props = properties.get('id', {})
+            opid_enum = opid_props.get('enum',[])
+            if opid_enum:
+                opid = opid_enum[0]
+            else:
+                opid = op
+
+            unique = properties.get('unique', False)
+            operations[opid] = {
+                "title": op,
                 "description": props.get('description',''),
-                "properties": props.get('properties',{}),
+                "properties": properties,
                 "additionalProperties": props.get('additionalProperties',False),
-                "unique": props.get('unique', False)
+                "unique": unique
             }
 
         return operations
