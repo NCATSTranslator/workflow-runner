ef06dc965316faee200764132737b4fdff4c8c8f
Abrar Mesbah <abrar.mesbah@gmail.com>
2022-06-09 09:16:09 -0400

    bump version

--- app/server.py
+++ app/server.py
@@ -24,7 +24,7 @@ LOGGER = logging.getLogger(__name__)
 
 openapi_args = dict(
     title="Workflow runner",
-    version="1.4.3",
+    version="1.4.4",
     terms_of_service="",
     translator_component="ARA",
     translator_teams=["Standards Reference Implementation Team"],
