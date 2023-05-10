3cb34f487ee47c1753cc4d084f203cc3643c3e44
Max Wang <max@covar.com>
2023-05-10 16:03:12 -0400

    Set wfr to use its own maturity as service maturities

--- app/server.py
+++ app/server.py
@@ -230,7 +230,7 @@ async def refresh_services_and_operations():
     global SERVICES
     # Start with empty SERVICES dict.
     SERVICES = defaultdict(list)
-    endpoints = SmartAPI(SERVICES_MATURITY, TRAPI_VERSION).get_operations_endpoints()
+    endpoints = SmartAPI(OPENAPI_SERVER_MATURITY, TRAPI_VERSION).get_operations_endpoints()
     for endpoint in endpoints:
         try:
             base_url = parse_obj_as(HttpUrl, endpoint["url"])
