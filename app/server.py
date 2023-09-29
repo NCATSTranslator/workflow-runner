eecb313b30566598018cf56deebbdf99e6b2cfee
Max Wang <max@covar.com>
2023-09-29 11:06:06 -0400

    Bump patch version

--- app/server.py
+++ app/server.py
@@ -22,7 +22,7 @@ LOGGER = logging.getLogger(__name__)
 
 openapi_args = dict(
     title="Workflow runner",
-    version="1.6.4",
+    version="1.6.5",
     terms_of_service="",
     translator_component="ARA",
     translator_teams=["Standards Reference Implementation Team"],
